9805d89d00f3cd1ad251040b36d04ee1dec72f3b
imdurgeshpal <imdurgeshpal@gmail.com>
2017-07-26 14:46:05 +0530

    All CRUD Operations

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import{UserService} from './user.service';
 import  {User}from './user';
-
+import {clone} from 'lodash';
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
@@ -10,6 +10,11 @@ import  {User}from './user';
 export class UserComponent implements OnInit {
 
   users:User[];
+  userForm:boolean=false;
+  isNewUser:boolean;
+  newUser:any={};
+  editUserForm:boolean=false;
+  editedUser:any={};
   constructor(private userService:UserService) { }
 
   ngOnInit() {
@@ -19,4 +24,49 @@ export class UserComponent implements OnInit {
     this.users=this.userService.getUsersFromData();
   }
 
+  showEditUserForm(user:User){
+    if(!user){
+      this.userForm=false;
+      return;
+    }
+    this.editUserForm=true;
+    this.editedUser=clone(user);
+
+
+  }
+  showAddUserForm(){
+
+    // resets form if edited user
+    if(this.users.length){
+      this.newUser={};
+    }
+    this.userForm=true;
+    this.isNewUser=true;
+
+  }
+  saveUser(user:User){
+    if(this.isNewUser){
+      //add a new user
+      this.userService.addUser(user);
+    }
+    this.userForm=false;
+  }
+  updateUser(){
+    this.userService.updateUser(this.editedUser);
+    this.editUserForm=false;
+    this.editedUser={};
+  }
+
+  removeUser(user:User){
+    this.userService.deleteUser(user);
+  }
+  cancelEdits(){
+    this.editedUser={};
+    this.editUserForm=false;
+  }
+
+  cancelNewUser(){
+    this.newUser={};
+    this.userForm=false;
+  }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,8 +1,7 @@
 import { Injectable } from '@angular/core';
 import  {User} from './user';
 import {USER_PERSONS} from './user-data';
-
-
+import { findIndex } from 'lodash';
 @Injectable()
 export class UserService {
   private upersons=USER_PERSONS;
@@ -10,6 +9,22 @@ export class UserService {
   getUsersFromData():User[]{
     console.log(this.upersons);
     return this.upersons;
+  }
+
+  addUser(user:User){
+    this.upersons.push(user);
+    console.log(this.upersons);
+
+  }
+  updateUser(user:User){
+    let index=findIndex(this.upersons,(u:User)=>{
+      return u.id=== user.id;
+    })
+    this.upersons[index]=user;
+  }
+  deleteUser(user:User){
+    this.upersons.splice(this.upersons.indexOf(user),1);
+    console.log(this.upersons);
   }
 
   constructor() { }
